d383c422fcd5d3ba551e89d3b4d9b464838aefdb
lixuan12 <lixuan12@kuaishou.com>
2024-12-27 19:58:08 +0800

    feat: 完成一些练习

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,17 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    if n == 0 || n == 1 {
+        result
+    } else {
+        let mut i = 2;
+        while i <= n {
+            result *= i;
+            i += 1;
+        }
+        result
+    }
 }
 
 #[cfg(test)]
